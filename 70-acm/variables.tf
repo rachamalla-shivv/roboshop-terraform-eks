0eca86c35b661d59b003c33ec6f1110400d8beb8
rachamalla-shivv <rachamalla.shivv@gmail.com>
2026-08-15 16:11:28 +0530

    IAC-EKS

---
+++ 70-acm/variables.tf
@@ -0,0 +1,15 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z01771181AKUPZX8DM2VP"
+}
+
+variable "domain_name" {
+    default = "shivv-aws.fun"
+}
---
+++ 90-eks/variables.tf
@@ -0,0 +1,35 @@
+variable "project_name" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z01771181AKUPZX8DM2VP"
+}
+
+variable "domain_name" {
+    default = "shivv-aws.fun"
+}
+
+variable "eks_version" {
+
+}
+
+variable "eks_nodegroup_blue_version" {
+
+}
+
+variable "eks_nodegroup_green_version" {
+
+}
+
+variable enable_blue {
+
+}
+
+variable enable_green {
+    
+}
